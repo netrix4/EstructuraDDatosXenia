a3fda1db33fbf5eab0cda7430b88faf4375b9d14
netrix4 <98143179+netrix4@users.noreply.github.com>
2022-11-29 18:49:56 -0800

    Delete recipeBinder.js

--- Estructura de datos/ProyectoRecetas/recipeBinder.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-export const pokeSearch = async (pokemonName) => {
-  const response = await fetch('https://pokeapi.co/api/v2/pokemon/' + pokemonName);
-  const data = await response.json();
-  return data;
-};
