2ba81ba27ecc3ab232bf3819d329bcdf83dfac41
netrix4 <98143179+netrix4@users.noreply.github.com>
2022-11-29 22:51:32 -0800

    Final Commit

--- Estructura de datos/ProyectoRecetas/indexScript.js
+++ Estructura de datos/ProyectoRecetas/indexScript.js
@@ -4,17 +4,82 @@ console.log('Testin de consola');
 
 import { recipeSearch } from './recipeFetcher.js';
 
+FillWithNoContent();
+let recipesList = null;
+
 async function getRecipes(food) {
   return await recipeSearch(food.toLowerCase());
 }
 
-const apiResponse = await getRecipes('pizza');
+function FillWithNoContent() {
+  const recipesContainer = document.getElementById('recipesContainer')
+  recipesContainer.innerHTML = '';
 
-console.log(apiResponse);
+  const noContentContainer = document.createElement('div')
+  noContentContainer.classList.add('noContentContainer')
 
-apiResponse.data.recipes.forEach(recipe => {
-  CreateRecipesCards(recipe);
-});
+  const h1NoContentTitle = document.createElement('h1')
+  h1NoContentTitle.textContent = 'Haz una busqueda para empezar'
+  
+  const imgNoContent = document.createElement('img')
+  imgNoContent.src = './resources/No-Content.png'
+  imgNoContent.classList.add('noContentImage')
+
+  noContentContainer.append(h1NoContentTitle, imgNoContent);
+  recipesContainer.append(noContentContainer);
+}
+
+async function onSearchButtonClick() {
+  const recipeToSearch = document.getElementById('txtSearch').value
+  const apiResponse = await getRecipes(recipeToSearch);
+  recipesList = apiResponse
+
+  console.log(apiResponse);
+
+  const recipesContainer = document.getElementById('recipesContainer')
+  recipesContainer.innerHTML = '';
+
+  if (apiResponse.results) {
+    apiResponse.data.recipes.forEach(recipe => {
+      CreateRecipesCards(recipe);
+      
+    });
+    
+  } else {
+    FillWithNoContent();
+  }
+}
+function onClearButtonClick() {
+  const recipeToSearch = document.getElementById('txtSearch')
+  recipeToSearch.value = '';
+  FillWithNoContent();
+  
+}
+
+const onViewMoreClick = (recipe) => () =>{
+  console.log(recipe);
+  const detailsModal = document.getElementById('detailsModal')
+  const modalInformationContainer = document.createElement('div')
+  modalInformationContainer.classList.add('modalInformationContainer');
+
+  const h1 = document.createElement('h1');
+  const img = document.createElement('img');
+  const parraph = document.createElement('p')
+  const btnCloseModal = document.createElement('button');
+  
+  btnCloseModal.textContent = 'Cerrar';
+  btnCloseModal.classList.add('btnSearch');
+  h1.textContent = recipe.title;
+  img.src = recipe.image_url;
+  parraph.textContent = recipe.publisher
+  img.classList.add('modalImage')
+  btnCloseModal.onclick = () => window.detailsModal.close();
+
+  detailsModal.innerHTML = '';
+  modalInformationContainer.append(h1, img, parraph, btnCloseModal);
+  detailsModal.append(modalInformationContainer);
+  detailsModal.show();
+}
 
 function CreateRecipesCards(recipe){
   const singleRecipe = document.createElement('div')
@@ -30,6 +95,7 @@ function CreateRecipesCards(recipe){
   const dimmedText = document.createElement('div')
   dimmedText.classList.add('dimmedText')
   dimmedText.textContent = 'Ver Más'
+  dimmedText.onclick = onViewMoreClick(recipe);
   trasition.append(dimmedText);
   
   const recipeDescription = document.createElement('div');
@@ -48,3 +114,9 @@ function CreateRecipesCards(recipe){
   recipesContainer.append(singleRecipe);
   
 }
+
+const searchButton = document.getElementById('btnSearch');
+const clearButton = document.getElementById('btnClear');
+
+clearButton.onclick = onClearButtonClick;
+searchButton.onclick = onSearchButtonClick;
